c67e49bad62508f97b13506c28a76efad58998a8
hunterhusker <hunterhusker@gmail.com>
2022-03-08 23:14:50 -0600

    added internal minHeap for each screen

--- mapBuilder.h
+++ mapBuilder.h
@@ -16,20 +16,12 @@ typedef struct cell
     bool inHeap;
 } cell_t;
 
-typedef struct map
-{
-    cell_t map[21][80];
-    cell_t *eMap[21][80];
-    int exits[4][2];
-} map_t;
-
 /// Heap structs so that they can be used in here
 typedef struct heapNode
 {
     cell_t  *data;
-    //void *data;
     int *value;
-    int x, y;
+    //int x, y;
 } heapNode_t;
 
 typedef struct minHeap
@@ -37,6 +29,14 @@ typedef struct minHeap
     heapNode_t heap[1860];
     int currLen;
 } minHeap_t;
+
+typedef struct map
+{
+    cell_t map[21][80];
+    cell_t *eMap[21][80];
+    int exits[4][2];
+    minHeap_t mh;
+} map_t;
 
 // Map Methods
 void printMap(map_t *screen);
