0034c824796ea8fa4905b8facaf22c456ee0bf10
hunterhusker <hunterhusker@gmail.com>
2022-03-29 23:04:56 -0500

    worked on making it a class

--- minHeap.h
+++ minHeap.h
@@ -2,17 +2,38 @@
 #define MINHEAP_H
 
 #include "mapBuilder.h" // allows us to return cell_t and take it as inputs
+#include <vector>
 
-void mhSwap(minHeap_t *mh, int idx1, int idx2);
-void heapifyUpCell(minHeap_t *mh, cell_t *cell);
-void heapifyUp(minHeap_t *mh, int srcIdx);
-void heapifyDown(minHeap_t *mh, int srcIdx);
-void mhAdd(minHeap_t *mh, cell_t *cell);
-cell_t mhExtract(minHeap_t *mh);
-int mhFind(minHeap_t *mh, cell_t *cell);
-void mhResetMap(cell_t map[21][80]);
-void mhAddAll(minHeap_t *mh, cell_t map[21][80]);
-void mhDeleteElement(minHeap_t *mh, cell_t *theDoomed);
-cell_t *peek(minHeap_t *mh);
+/// Heap structs so that they can be used in here
+typedef struct heapNode
+{
+    cell_t  *data;
+    int *value;
+    int x, y;
+} heapNode_t;
+
+class minHeap {
+private:
+    //heapNode_t heap[1860];
+    std::vector<heapNode> heap;
+    int currLen;
+
+public:
+    minHeap();
+
+    void mhSwap(int idx1, int idx2);
+    void heapifyUpCell(cell_t *cell);
+    void heapifyUp(int srcIdx);
+    void heapifyDown(int srcIdx);
+    void mhAdd(cell_t *cell);
+    cell_t mhExtract();
+    int mhFind(cell_t *cell);
+    void mhResetMap(cell_t map[21][80]);
+    void mhAddAll(cell_t map[21][80]);
+    void mhDeleteElement(cell_t *theDoomed);
+    cell_t *peek();
+};
+
+
 
 #endif
