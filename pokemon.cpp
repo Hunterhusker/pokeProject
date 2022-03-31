f2de217f7b9eff8600dd25a4c09b4bb8a87ac5cf
hunterhusker <hunterhusker@gmail.com>
2022-03-31 03:59:05 -0500

    added pokemon class object

--- pokemon.cpp
+++ pokemon.cpp
@@ -3,3 +3,18 @@
 //
 
 #include "pokemon.h"
+
+pokemon::pokemon(int id, std::string identifier, int species_id, int height, int weight, int base_experience, int order, int isDefault) {
+    this->id = id;
+    this->identifier = identifier;
+    this->species_id = species_id;
+    this->height = height;
+    this->weight = weight;
+    this->base_experience = base_experience;
+    this->order = order;
+    this->isDefault = isDefault;
+}
+
+std::ostream &operator<< (std::ostream &o, pokemon p) {
+    return o << p.identifier << " ID:" << p.id << " SpeciesID: " << p.species_id << " Height: " << p.height << " Weight: " << p.weight << " BaseExp: " << p.base_experience << " Order: " << p.order << " Default: " << p.isDefault;
+}
--- pokemon.h
+++ pokemon.h
@@ -1,12 +1,26 @@
 //
 // Created by hunter on 3/30/22.
 //
-
 #ifndef POKEPROJECT_POKEMON_H
 #define POKEPROJECT_POKEMON_H
 
-typedef struct pokemon {
+#include <iostream>
+#include <string>
 
-} pokemon_t;
+class pokemon {
+public:
+    int id;
+    std::string identifier;
+    int species_id;
+    int height;
+    int weight;
+    int base_experience;
+    int order;
+    int isDefault;
+
+    pokemon(int id, std::string identifier, int species_id, int height, int weight, int base_experience, int order, int isDefault);
+};
+
+std::ostream &operator<< (std::ostream &o, pokemon p);
 
 #endif //POKEPROJECT_POKEMON_H
