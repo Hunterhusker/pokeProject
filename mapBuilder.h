d648f3abc64fb01f86b2076ee6910690bca03a17
hunterhusker <hunterhusker@gmail.com>
2022-04-10 20:58:28 -0500

    added the new entity class

--- mapBuilder.h
+++ mapBuilder.h
@@ -16,24 +16,25 @@ public:
     bool inHeap;
 };
 
-class entity_cell : cell {
+class entity_cell : public cell {
 public:
     pokemon_entity *pkmns[6];
     ~entity_cell();
+    entity_cell() {}
+    entity_cell(int x, int y, char type, int weight, int dist);
 };
 
 class map {
 public:
     cell map[21][80];
-    cell *eMap[21][80];
+    entity_cell *eMap[21][80];
     int exits[4][2];
     minHeap mh;
 };
 
 // Map Methods
-void printMap(map *screen);
-void emptyEntityMap(cell *eMap[21][80]);
-void deleteAllEntities(cell *eMap[21][80]);
+void emptyEntityMap(entity_cell *eMap[21][80]);
+void deleteAllEntities(entity_cell *eMap[21][80]);
 void emptyGrid(cell map[21][80]);
 void placeExits(cell map[21][80], int exits[4][2], int e1, int e2, int e3, int e4);
 int placeBiomes(cell map[21][80], int filledTiles[][2]);
@@ -47,10 +48,10 @@ void generate(int e1, int e2, int e3, int e4, map *board, int shopChance, int en
 int placeEntities(int entityCount, map *screen, minHeap *mh);
 
 // Entity methods
-cell* placeEntity(map *screen, minHeap *mh, char type);
-void delEntity(map *screen, minHeap *mh, cell *entity);
-int moveEntity(map *screen, minHeap *mh, cell *entity, cell *player);
-int fightPLayer(map *screen, cell *entity, cell *player);
+entity_cell* placeEntity(map *screen, minHeap *mh, char type);
+void delEntity(map *screen, minHeap *mh, entity_cell *entity);
+int moveEntity(map *screen, minHeap *mh, entity_cell *entity, entity_cell *player);
+int fightPLayer(map *screen, entity_cell *entity, entity_cell *player);
 void printAmogus();
 
 #endif
