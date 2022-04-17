c0de729f9eeb2b9bb00e9468e4ab9f5da3f07a77
hunterhusker <hunterhusker@gmail.com>
2022-04-17 00:51:21 -0500

    global extern declaration so we can use the read in data anywhere

---
+++ global.h
@@ -0,0 +1,16 @@
+#ifndef POKEPROJECT_GLOBAL_H
+#define POKEPROJECT_GLOBAL_H
+
+#include "pokemon.h"
+#include <vector>
+
+extern std::vector<pokemon> pokeList;
+extern std::vector<pokemon_species> species;
+extern std::vector<experience> expList;
+extern std::vector<type_names> types;
+extern std::vector<pokemon_moves> pkmnMoves;
+extern std::vector<moves> mvList;
+extern std::vector<pokemon_stats> pkmnStats;
+extern std::vector<stats> statList;
+
+#endif //POKEPROJECT_GLOBAL_H
