9e98e689c093a0bcae92b8b776f46e34d0bf3608
hunterhusker <hunterhusker@gmail.com>
2022-04-20 01:44:58 -0500

    added a new global vector for the new data

--- global.h
+++ global.h
@@ -10,7 +10,8 @@ extern std::vector<experience> expList;
 extern std::vector<type_names> types;
 extern std::vector<pokemon_moves> pkmnMoves;
 extern std::vector<moves> mvList;
-extern std::vector<pokemon_stats> pkmnStats;
+extern std::vector<pokemon_stats> pkStats;
 extern std::vector<stats> statList;
+extern std::vector<pokemon_types> pkmnTypes;
 
 #endif //POKEPROJECT_GLOBAL_H
